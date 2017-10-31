0b3ca4452432be6cbb87e813206b7c597b9e69ed
TimothyRager <important.rager.activities@gmail.com>
2017-10-31 12:22:46 -0400

    Update Human.java

--- src/main/java/io/zipcoder/threedaystodeliver/humanresourceapp/Human.java
+++ src/main/java/io/zipcoder/threedaystodeliver/humanresourceapp/Human.java
@@ -1,4 +1,12 @@
 package io.zipcoder.threedaystodeliver.humanresourceapp;
 
 public class Human {
+  
+  ContactInfo contact;
+  Address address;
+  
+  public Human(){}
+  
+  public Human(??);
+  
 }
