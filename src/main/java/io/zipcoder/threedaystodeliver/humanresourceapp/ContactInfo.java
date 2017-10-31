c33160ad9218aaa909818071e98f04f713ea9920
TimothyRager <important.rager.activities@gmail.com>
2017-10-31 12:21:38 -0400

    Create ContactInfo.java

---
+++ src/main/java/io/zipcoder/threedaystodeliver/humanresourceapp/ContactInfo.java
@@ -0,0 +1,28 @@
+class ContactInfo{
+
+  String name;
+  Address address;
+  String phone;
+  String email;
+  
+  public ContactInfo(){}
+  public ContactInfo(String name, 
+                     Address address, 
+                     String phone,
+                     String email){
+    this.name=name;
+    this.Address=Address;
+    this.phone=phone;
+    this.email=email;
+  }
+  
+  public void setName(String name){}
+  public String getName(){}
+  
+  public void setPhone(String phone){}
+  public String getPhone(){}
+  
+  public void setEmail(String email){}
+  public String getEmail(){}
+  
+}
