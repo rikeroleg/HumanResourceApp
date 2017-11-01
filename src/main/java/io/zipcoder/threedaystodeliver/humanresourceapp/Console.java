ee52c62360ef2158ae2f92dc59798abf5d83b125
katherine <katherine@zipcoders-MacBook-Pro.local>
2017-11-01 19:39:33 -0400

    working on console

--- src/main/java/io/zipcoder/threedaystodeliver/humanresourceapp/Console.java
+++ src/main/java/io/zipcoder/threedaystodeliver/humanresourceapp/Console.java
@@ -1,11 +1,12 @@
 package io.zipcoder.threedaystodeliver.humanresourceapp;
 
+import java.util.Date;
 import java.util.Scanner;
 
 public class Console {
 
     public static void main(String[] args) {
-
+        Console console = new Console();
 
     }
 
@@ -48,6 +49,42 @@ public class Console {
         Person newProspect = PersonHandler.createProspect(requestedInfo);
         currentPerson = newProspect;
         printCurrentPerson();
+    }
+
+    public void hirePerson() {
+        System.out.print("Enter hire date year: ");
+        int inputHireDateYear = scan.nextInt();
+        System.out.print("Enter hire date month: ");
+        int inputHireDateMonth = scan.nextInt();
+        System.out.print("Enter hire date day: ");
+        int inputHireDateDay = scan.nextInt();
+        scan.next();
+        System.out.println("Enter job title: ");
+        String inputJobTitle = scan.nextLine();
+        System.out.println("Enter paid Monthly/Hourly/Project");
+        System.out.print("Enter salary: ");
+        double inputSalary = scan.nextDouble();
+        System.out.print("Enter bonus: ");
+        double inputBonus = scan.nextDouble();
+        System.out.print("Enter PTO for the year: ");
+        double inputPTO = scan.nextDouble();
+        scan.next();
+        System.out.print("Opt in to medical coverage? y/n: ");
+        String inputMedical = scan.nextLine();
+        System.out.print("Opt in to dental coverage? y/n: ");
+        String inputDental = scan.nextLine();
+        System.out.print("Opt in to vision coverage? y/n: ");
+        String inputVision = scan.nextLine();
+        System.out.print("Opt in to prescription coverage? y/n: ");
+        String inputPrescription = scan.nextLine();
+        System.out.print("Enter retirement match %: ");
+        double inputRetirementMatch = scan.nextDouble();
+
+        Date hireDate = new Date(inputHireDateYear, inputHireDateMonth, inputHireDateDay);
+        Compensation newCompensation = new Compensation();
+        newCompensation.setBonus(inputBonus);
+        newCompensation.setTypeAndAmount();
+
     }
 
 
