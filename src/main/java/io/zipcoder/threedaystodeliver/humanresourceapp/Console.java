3087274e0753bf71b487c0e98952ce635259ff6e
katherine <katherine@zipcoders-MacBook-Pro.local>
2017-11-01 18:35:07 -0400

    working on console

--- src/main/java/io/zipcoder/threedaystodeliver/humanresourceapp/Console.java
+++ src/main/java/io/zipcoder/threedaystodeliver/humanresourceapp/Console.java
@@ -1,9 +1,38 @@
 package io.zipcoder.threedaystodeliver.humanresourceapp;
 
+import java.util.Scanner;
+
 public class Console {
 
     public static void main(String[] args) {
 
 
     }
+
+    public Person currentPerson;
+
+    //methods for all employee types
+    public HrContactInfo inputContactInfo() {
+        Scanner scan = new Scanner(System.in);
+
+        return null;
+    }
+
+    public void printCurrentPerson() {
+        System.out.println(currentPerson);
+    }
+
+
+    // prospect methods
+    public void addNewProspect() {
+        HrContactInfo requestedInfo = inputContactInfo();
+        Person newProspect = PersonHandler.createProspect(requestedInfo);
+        currentPerson = newProspect;
+        printCurrentPerson();
+    }
+
+
+
+
+
 }
