3c2f307c26fb5129402819cb8f46de42f5a7e1d7
katherine <katherine@zipcoders-MacBook-Pro.local>
2017-11-01 18:45:39 -0400

    added input contact info method

--- src/main/java/io/zipcoder/threedaystodeliver/humanresourceapp/Console.java
+++ src/main/java/io/zipcoder/threedaystodeliver/humanresourceapp/Console.java
@@ -10,12 +10,31 @@ public class Console {
     }
 
     public Person currentPerson;
+    Scanner scan = new Scanner(System.in);
 
     //methods for all employee types
     public HrContactInfo inputContactInfo() {
-        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter name: ");
+        String inputName = scan.nextLine();
+        System.out.print("Enter address line 1: ");
+        String inputAddressLine1 = scan.nextLine();
+        System.out.print("Enter address line 2: ");
+        String inputAddressLine2 = scan.nextLine();
+        System.out.print("Enter city: ");
+        String inputCity = scan.nextLine();
+        System.out.print("Enter state: ");
+        String inputState = scan.nextLine();
+        System.out.print("Enter zip code: ");
+        String inputZipCode = scan.nextLine();
+        System.out.print("Enter phone number: ");
+        String inputPhoneNumber = scan.nextLine();
+        System.out.print("Enter email address: ");
+        String inputEmailAddress = scan.nextLine();
 
-        return null;
+        StreetAddress newStreetAddress = new StreetAddress(inputAddressLine1, inputAddressLine2, inputCity, inputState, inputZipCode);
+        HrContactInfo newHrContactInfo = new HrContactInfo(inputName, newStreetAddress, inputPhoneNumber, inputEmailAddress);
+
+        return newHrContactInfo;
     }
 
     public void printCurrentPerson() {
