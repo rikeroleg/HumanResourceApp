9427c582fb22339f80bd1e9cd03250d33d44e9b2
pavel <Pavelparfenov112@gmail.com>
2017-11-01 16:34:48 -0400

    "PersonHandler implemented create prospect, hire prospect, hire off the street, and terminate"

--- src/main/java/io/zipcoder/threedaystodeliver/humanresourceapp/PersonHandler.java
+++ src/main/java/io/zipcoder/threedaystodeliver/humanresourceapp/PersonHandler.java
@@ -1,5 +1,7 @@
 package io.zipcoder.threedaystodeliver.humanresourceapp;
 
+import java.util.Date;
+
 public class PersonHandler {
 
     public static Person createProspect(HrContactInfo hrContactInfo){
@@ -9,12 +11,34 @@ public class PersonHandler {
         return prospect;
     }
 
-    public static Person hire(Person person){
-        return null;
+    public static Person hireFromProspect(Person person, Date date, Compensation compensation){
+        Person employee = person;
+        employee.setEmploymentStatus(EmploymentStatus.EMPLOYEE);
+        employee.setHiredDate(date);
+        employee.setCompensation(compensation);
+
+        return employee;
     }
 
-    public static Person terminate(Person person) {
-        return null;
+    public static Person hireFromHrInfo(HrContactInfo info, Date date, Compensation compensation){
+        Person employee = PersonFactory.createPerson(info);
+        employee.setEmploymentStatus(EmploymentStatus.EMPLOYEE);
+        employee.setHiredDate(date);
+        employee.setCompensation(compensation);
+
+        return employee;
+    }
+
+    public static Person terminate(Person person, Date terminationDate, String reasonForTermination, String exitInterview, Compensation postEmployComp) {
+        Person formerEmployee = person;
+        formerEmployee.setTerminationDate(terminationDate);
+        formerEmployee.setReasonForTermination(reasonForTermination);
+        formerEmployee.setExitInterview(exitInterview);
+        formerEmployee.setPostEmploymentComp(postEmployComp);
+        formerEmployee.setEmploymentStatus(EmploymentStatus.TERMINATED);
+        formerEmployee.setCompensation(null);
+
+        return formerEmployee;
     }
 
     public static Person promote(Person person, String newTitle, double newpayRate){
--- src/test/java/io/zipcoder/threedaystodeliver/humanresourceapp/PersonHandlerTest.java
+++ src/test/java/io/zipcoder/threedaystodeliver/humanresourceapp/PersonHandlerTest.java
@@ -3,14 +3,9 @@ package io.zipcoder.threedaystodeliver.humanresourceapp;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class PersonHandlerTest {
+import java.util.Date;
 
-//    Create a prospect ->
-//    enter information into console
-//    create person with entered contact info,
-//    add person to warehouse,
-//    verify person is in the warehouse
-//    information is populated into person
+public class PersonHandlerTest {
 
     String name = "Uncle Bob";
     StreetAddress address = new StreetAddress();
@@ -19,35 +14,70 @@ public class PersonHandlerTest {
 
     HrContactInfo info = new HrContactInfo(name, address, phone, email);
 
-
     @Test
     public void createProspectTypeShouldBeProspectTest(){
-        Person person = PersonHandler.createProspect(info);
-        Assert.assertEquals( EmploymentStatus.PROSPECT, person.getEmploymentStatus());
+        Person prospect = PersonHandler.createProspect(info);
+        Assert.assertEquals( EmploymentStatus.PROSPECT, prospect.getEmploymentStatus());
     }
 
     @Test
     public void createProspectAddToWarehouseTest(){
-//        PersonWarehouse.
+        PersonWarehouse.getInstance();
+        int beforeCount = PersonWarehouse.getAllPeople().size();
+        int expected = 5;
+
+        for (int i = 0; i < 5; i++) {
+            Person person = PersonHandler.createProspect(info);
+        }
+
+        int afterCount = PersonWarehouse.getAllPeople().size();
+        int actual = afterCount - beforeCount;
+
+        Assert.assertEquals(expected, actual);
     }
 
 //    Change prospects status during interview stage ->
 //    enter information into console
 //    infomation updates in person object
-//
+////
+//    @Test
+//    public void
+
+
 //    Hire employee from prospect ->
 //    enter information into console
 //    populate employee fields (start date, salary, etc.)
 //
-//    Terminate employee ->
-//    enter information into console
-//    populate former employee fields,
-//    remove from current employee lists
-//
-//    Promote employee ->
-//    enter information into console
-//    update job title, compensation
-//
+
+    @Test
+    public void hireProspectChangeTypeTest(){
+        Person prospect = PersonHandler.createProspect(info);
+        Person employee = PersonHandler.hireFromProspect(prospect, new Date(), new Compensation());
+
+        Assert.assertEquals(EmploymentStatus.EMPLOYEE, employee.getEmploymentStatus());
+    }
+
+    @Test
+    public void hireProspectHasDateTest(){
+        Person prospect = PersonHandler.createProspect(info);
+        Date date = new Date();
+        Person employee = PersonHandler.hireFromProspect(prospect, date, new Compensation());
+
+        Assert.assertEquals(date, employee.getHiredDate());
+    }
+
+    @Test
+    public void hireProspectHasCompensationTest(){
+        Person prospect = PersonHandler.createProspect(info);
+        Compensation compensation = new Compensation();
+        compensation.setTypeAndAmount(Compensation.compensationType.Monthly, 2.50);
+        Person employee = PersonHandler.hireFromProspect(prospect, new Date(), compensation);
+
+        Assert.assertEquals(Compensation.compensationType.Monthly, employee.getCompensation().getCompensationType());
+        Assert.assertEquals(2.50, employee.getCompensation().getPayrate(), 0.001);
+    }
+
+    //
 //    Hire employee from new ->
 //    enter information into console
 //    create person with entered contact info,
@@ -55,6 +85,98 @@ public class PersonHandlerTest {
 //    verify person is in the warehouse
 //    information is populated into person
 //
+
+    @Test
+    public void hireFromStreetTypeTest(){
+        Person employee = PersonHandler.hireFromHrInfo(info, new Date(), new Compensation());
+
+        Assert.assertEquals(EmploymentStatus.EMPLOYEE, employee.getEmploymentStatus());
+    }
+
+    @Test
+    public void hireFromStreetHasDateTest(){
+        Date date = new Date();
+        Person employee = PersonHandler.hireFromHrInfo(info, new Date(), new Compensation());
+
+        Assert.assertEquals(date, employee.getHiredDate());
+    }
+
+    @Test
+    public void hireFromStreetHasCompensationTest(){
+        Compensation compensation = new Compensation();
+        compensation.setTypeAndAmount(Compensation.compensationType.Hourly, 0.50);
+
+        Person employee = PersonHandler.hireFromHrInfo(info, new Date(), compensation);
+
+        Assert.assertEquals(Compensation.compensationType.Hourly, employee.getCompensation().getCompensationType());
+        Assert.assertEquals(0.50, employee.getCompensation().getPayrate(), 0.001);
+    }
+
+//    Terminate employee ->
+//    enter information into console
+//    populate former employee fields,
+//    remove from current employee lists
+//
+
+    @Test
+    public void terminateEmployeeTypeTest(){
+        Person employee = PersonHandler.hireFromHrInfo(info, new Date(), new Compensation());
+        Person formerEmployee = PersonHandler.terminate(employee, new Date(), "", "", new Compensation());
+
+        Assert.assertEquals(EmploymentStatus.TERMINATED, formerEmployee.getEmploymentStatus());
+    }
+
+    @Test
+    public void terminateEmployeePayRateIsNullTest(){
+        Person employee = PersonHandler.hireFromHrInfo(info, new Date(), new Compensation());
+        Person formerEmployee = PersonHandler.terminate(employee, new Date(), "", "", new Compensation());
+
+        Assert.assertNull(formerEmployee.getCompensation());
+    }
+
+    @Test
+    public void terminatedEmployeePostCompIsNotNullTest(){
+        Compensation postComp = new Compensation();
+        postComp.setRetirement(true);
+
+        Person employee = PersonHandler.hireFromHrInfo(info, new Date(), new Compensation());
+        Person formerEmployee = PersonHandler.terminate(employee, new Date(), "", "", postComp);
+
+        Assert.assertEquals(true, formerEmployee.getPostEmploymentComp().isRetirement());
+    }
+
+    @Test
+    public void terminatedEmployeeHasExitInterviewTest(){
+        String exit = "TERMINATED";
+        Person employee = PersonHandler.hireFromHrInfo(info, new Date(), new Compensation());
+        Person formerEmployee = PersonHandler.terminate(employee, new Date(), "", exit, new Compensation());
+
+        Assert.assertTrue(exit.equals(formerEmployee.getExitInterview()));
+    }
+
+    @Test
+    public void terminatedEmployeeHasTermDateTest(){
+        Date date = new Date();
+        Person employee = PersonHandler.hireFromHrInfo(info, new Date(1, 1,1 ), new Compensation());
+        Person formerEmployee = PersonHandler.terminate(employee, date, "", "", new Compensation());
+
+        Assert.assertEquals(date, employee.getTerminationDate());
+    }
+
+    @Test
+    public void terminatedEmployeeHasReasonForTermination(){
+        String reason = "code wasn't clean";
+
+        Person employee = PersonHandler.hireFromHrInfo(info, new Date(), new Compensation());
+        Person formerEmployee = PersonHandler.terminate(employee, new Date(), reason, "", new Compensation());
+
+        Assert.assertEquals(reason, employee.getReasonForTermination());
+    }
+
+//    Promote employee ->
+//    enter information into console
+//    update job title, compensation
+
 //    //Employee record object - arraylist of [all employee fields], date stamp on each one,
 //    every time a field is updated, a new entry is added to arraylist
 //    Update contact info ->
