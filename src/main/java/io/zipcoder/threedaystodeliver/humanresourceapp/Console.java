7fa6eb690ac5f63382c01a4460bf4ed11be8855b
katherine <katherine@zipcoders-MacBook-Pro.local>
2017-11-02 15:29:40 -0400

    more methods on console

--- src/main/java/io/zipcoder/threedaystodeliver/humanresourceapp/Console.java
+++ src/main/java/io/zipcoder/threedaystodeliver/humanresourceapp/Console.java
@@ -149,8 +149,34 @@ public class Console {
 
     }
 
-    public String getInput() {
-        return null;
+    public void addNewProspect() {
+        HrContactInfo requestedInfo = inputAllContactInfo();
+        Person newProspect = PersonHandler.createProspect(requestedInfo);
+        currentPerson = newProspect;
+    }
+
+    public HrContactInfo inputAllContactInfo() {
+        System.out.print("Enter name: ");
+        String inputName = getInput();
+        System.out.print("Enter address line 1: ");
+        String inputAddressLine1 = getInput();
+        System.out.print("Enter address line 2: ");
+        String inputAddressLine2 = getInput();
+        System.out.print("Enter city: ");
+        String inputCity = getInput();
+        System.out.print("Enter state: ");
+        String inputState = getInput();
+        System.out.print("Enter zip code: ");
+        String inputZipCode = getInput();
+        System.out.print("Enter phone number: ");
+        String inputPhoneNumber = getInput();
+        System.out.print("Enter email address: ");
+        String inputEmailAddress = getInput();
+
+        StreetAddress newStreetAddress = new StreetAddress(inputAddressLine1, inputAddressLine2, inputCity, inputState, inputZipCode);
+        HrContactInfo newHrContactInfo = new HrContactInfo(inputName, newStreetAddress, inputPhoneNumber, inputEmailAddress);
+
+        return newHrContactInfo;
     }
 
     /*
@@ -182,39 +208,6 @@ public class Console {
         PersonWarehouse personWarehouse = new PersonWarehouse();
         //System.out.println(personWarehouse.getPersonByName());
     }
-    */
-
-
-
-    // method - add new prospect, creates a prospect based on inputted contact info
-    // will need to create an HrContactInfo object and pass it to the person handler
-    // method that creates one:
-
-    /* methods that somehow got deleted
-
-    public HrContactInfo inputContactInfo() {
-        System.out.print("Enter name: ");
-        String inputName = scan.nextLine();
-        System.out.print("Enter address line 1: ");
-        String inputAddressLine1 = scan.nextLine();
-        System.out.print("Enter address line 2: ");
-        String inputAddressLine2 = scan.nextLine();
-        System.out.print("Enter city: ");
-        String inputCity = scan.nextLine();
-        System.out.print("Enter state: ");
-        String inputState = scan.nextLine();
-        System.out.print("Enter zip code: ");
-        String inputZipCode = scan.nextLine();
-        System.out.print("Enter phone number: ");
-        String inputPhoneNumber = scan.nextLine();
-        System.out.print("Enter email address: ");
-        String inputEmailAddress = scan.nextLine();
-
-        StreetAddress newStreetAddress = new StreetAddress(inputAddressLine1, inputAddressLine2, inputCity, inputState, inputZipCode);
-        HrContactInfo newHrContactInfo = new HrContactInfo(inputName, newStreetAddress, inputPhoneNumber, inputEmailAddress);
-
-        return newHrContactInfo;
-    }
 
     public void printCurrentPerson() {
         System.out.println(currentPerson);
@@ -222,13 +215,8 @@ public class Console {
 
 
     // prospect methods
-    public void addNewProspect() {
-        HrContactInfo requestedInfo = inputContactInfo();
-        Person newProspect = PersonHandler.createProspect(requestedInfo);
-        currentPerson = newProspect;
-        printCurrentPerson();
-    }
-     */
+
+
 
 
 
@@ -258,6 +246,8 @@ public class Console {
     }
 
     public void updatePhoneNumber(){
+
+    */
 
 }
 
