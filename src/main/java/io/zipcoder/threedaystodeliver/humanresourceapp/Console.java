5039e06e66f9e6a8acca70f47c8558bc4323c9a4
rikeroleg <rikeroleg@gmail.com>
2017-11-02 15:45:53 -0400

    some consol21

--- src/main/java/io/zipcoder/threedaystodeliver/humanresourceapp/Console.java
+++ src/main/java/io/zipcoder/threedaystodeliver/humanresourceapp/Console.java
@@ -3,107 +3,132 @@ package io.zipcoder.threedaystodeliver.humanresourceapp;
 import java.util.Scanner;
 
 public class Console {
+    static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to Human Resources Application Manager\nPlease Select an option: \n1.[Prospects]    2.[Employees]   3.[Print Reports]\n");
         int tier1 = scanner.nextInt();
-        switch (tier1){
-            case 1:
-                System.out.println("1.[Add New Prospect]   2.[Select Existing Prospect]");
-                int prospectTier2 = scanner.nextInt();
-                switch (prospectTier2){
-                    case 1:
-                        HrContactInfo();
-                        break;
-                    case 2:
-                        System.out.println("1.[Select By ID]   2.[Select By Name]");
-                        int prospectTier3 = scanner.nextInt();
-                        switch (prospectTier3) {
-                            case 1:
-                                selectById();
-                                System.out.println("1.[Update Contact Info]   2.[Hire This Prospect]");
-                                int prospectTier4 = scanner.nextInt();
-                                switch (prospectTier4) {
-                                    case 1:
-                                        //Update prospect contact Info
-                                        break;
-                                    case 2:
-                                        //Hire this prospect
-                                        break;
-                                }
+        mainMenu(tier1);
+    }
 
-                            case 2:
-                                selectByName();
-                                break;
-                        }
-                }
+    private static void mainMenu(int tier1){
+        switch (tier1) {
+            case 1:
+                System.out.println("1.[Add New Prospect]   2.[Select Existing Prospect]   3.[View All Prospects]");
+                int prospectTier2 = scanner.nextInt();
+                prospectMenu2(prospectTier2);
             case 2:
                 System.out.println("1.[Hire New Employee]   2.[Make Changes To Existing Employee]");
                 int employeeTier2 = scanner.nextInt();
-                switch (employeeTier2) {
-                    case 1:
-                        hireProspect();
-                        break;
-                    case 2:
-                        //Update Employee Info
-                        break;
-                }
+                employeeMenu2(employeeTier2);
             case 3:
                 // Print People
                 PersonWarehouse printPeople = new PersonWarehouse();
                 System.out.print(printPeople.getAllPeople());
                 break;
-            }
+        }
+    }
+
+    private static void employeeMenu2(int employeeTier2) {
+        switch (employeeTier2) {
+            case 1:
+                hireProspect();
+                break;
+            case 2:
+                //Update Employee Info
+                break;
+        }
+    }
+
+    private static void prospectMenu2(int prospectTier2) {
+        switch (prospectTier2) {
+            case 1:
+                HrContactInfo();
+                break;
+            case 2:
+                System.out.println("1.[Select By ID]   2.[Select By Name]");
+                int prospectTier3 = scanner.nextInt();
+                prospectMenu3(prospectTier3);
+            case 3:
+                //Print
+
+        }
+    }
+
+    private static void prospectMenu3(int prospectTier3) {
+        switch (prospectTier3) {
+            case 1:
+                selectById(prompt);
+                System.out.println("1.[Update Contact Info]   2.[Hire This Prospect]");
+                int prospectTier4 = scanner.nextInt();
+
+
+                prospectMenu4(prospectTier4);
+
+            case 2:
+                selectByName();
+                break;
+        }
+    }
+
+    private static void prospectMenu4(int prospectTier4) {
+        switch (prospectTier4) {
+            case 1:
+                //Update prospect contact Info
+                break;
+            case 2:
+                //Hire this prospect
+                break;
+        }
     }
 
 
-    public static String HrContactInfo(String prompt){
+    public static String HrContactInfo(String prompt) {
         Scanner scan = new Scanner(System.in);
         System.out.println(prompt);
         String in = scan.nextLine();
         return in;
     }
 
-    public static void HrContactInfo(){
-        HrContactInfo("Enter Name: ");
-        HrContactInfo("Enter Address Line 1: ");
-        HrContactInfo("Enter Address Line 2: ");
-        HrContactInfo("Enter City: ");
-        HrContactInfo("Enter State: ");
-        HrContactInfo("Enter Zip Code: ");
-        HrContactInfo("Enter Phone Number: ");
-        HrContactInfo("Enter Email Address: ");
+    public static void HrContactInfo() {
+        String name = HrContactInfo("Enter Name: ");
+        String address1 = HrContactInfo("Enter Address Line 1: ");
+        String address2 = HrContactInfo("Enter Address Line 2: ");
+        String city = HrContactInfo("Enter City: ");
+        String state = HrContactInfo("Enter State: ");
+        String zip = HrContactInfo("Enter Zip Code: ");
+        String phone = HrContactInfo("Enter Phone Number: ");
+        String email = HrContactInfo("Enter Email Address: ");
     }
 
-    public static void hireProspect(){
-        HrContactInfo("Enter Hire Date Year: ");
-        HrContactInfo("Enter Hire Date Month: ");
-        HrContactInfo("Enter Hire Date Day: ");
-        HrContactInfo("Enter Job Title: ");
-        HrContactInfo("Enter Paid Monthly/Hourly/Project");
-        HrContactInfo("Enter Salary: ");
-        HrContactInfo("Enter Bonus: ");
-        HrContactInfo("Enter PTO For The Year: ");
-        HrContactInfo("Opt In To Medical coverage? Y/N: ");
-        HrContactInfo("Opt In To Dental Coverage? Y/N: ");
-        HrContactInfo("Opt In To Vision Coverage? Y/N: ");
-        HrContactInfo("Opt In to Prescription Coverage? Y/N: ");
-        HrContactInfo("Enter Retirement Match %s: ");
+    public static void hireProspect() {
+        String hireYear = HrContactInfo("Enter Hire Date Year: ");
+        String hireMonth = HrContactInfo("Enter Hire Date Month: ");
+        String hireDay = HrContactInfo("Enter Hire Date Day: ");
+        String title = HrContactInfo("Enter Job Title: ");
+        String pay = HrContactInfo("Enter Paid Monthly/Hourly/Project");
+        String salary = HrContactInfo("Enter Salary: ");
+        String bonus = HrContactInfo("Enter Bonus: ");
+        String pto = HrContactInfo("Enter PTO For The Year: ");
+        String medical = HrContactInfo("Opt In To Medical coverage? Y/N: ");
+        String dental = HrContactInfo("Opt In To Dental Coverage? Y/N: ");
+        String vision = HrContactInfo("Opt In To Vision Coverage? Y/N: ");
+        String prescription = HrContactInfo("Opt In to Prescription Coverage? Y/N: ");
+        String retirement = HrContactInfo("Enter Retirement Match %s: ");
     }
 
-    public static void selectById(){
+    public static void selectById() {
         HrContactInfo("Enter ID: ");
-        PersonWarehouse personWarehouse = new PersonWarehouse();
+        PersonWarehouse personWarehouse = PersonWarehouse.getInstance().getPersonById();
         //System.out.println(personWarehouse.getPersonById());
     }
 
-    public static void selectByName(){
+    public static void selectByName() {
         HrContactInfo("Enter Name: ");
         PersonWarehouse personWarehouse = new PersonWarehouse();
         //System.out.println(personWarehouse.getPersonByName());
         //
-        }
+    }
 }
 
 
