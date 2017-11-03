71377b23a2eedc8d1b5c6b16d608bd8ec8551997
pavel <Pavelparfenov112@gmail.com>
2017-11-02 21:27:45 -0400

    "Menu stubs"

---
+++ src/main/java/io/zipcoder/threedaystodeliver/humanresourceapp/menus/Menu.java
@@ -0,0 +1,57 @@
+package io.zipcoder.threedaystodeliver.humanresourceapp.menus;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+/**
+ * Created by leon on 10/24/17.
+ * Yup, totally stole most of this from Leon, he makes good stuff.
+ */
+
+public abstract class Menu {
+
+    private final Enum[] menuOptions;
+
+    public Menu(Enum[] menuOptions) {
+        this.menuOptions = menuOptions;
+    }
+
+    abstract public void selectOption(String userInput);
+
+    public void display() {
+        String userInput;
+        do {
+            userInput = this.getUserInput().toUpperCase();
+            try {
+                selectOption(userInput);
+            } catch (IllegalArgumentException iae) {
+                handleIllegalArgumentException(userInput, iae);
+            } catch (NullPointerException npe) {
+                handleNullPointerException(npe);
+            }
+        } while (!"Exit".equalsIgnoreCase(userInput));
+    }
+
+    private void handleIllegalArgumentException(String illegalArgument, IllegalArgumentException iae) {
+        handleException("[ %s ] is an invalid argument.", illegalArgument);
+    }
+
+    private void handleNullPointerException(NullPointerException npe) {
+        handleException("Unable to retrieve data with input value.");
+    }
+
+    private void handleException(String message, Object... args) {
+        System.out.printf(message, args);
+    }
+
+    public String getUserInput() {
+        System.out.printf("===== %s =====", getClass().getSimpleName());
+        System.out.println("Select option:");
+        for (Enum e : menuOptions) {
+            System.out.printf("[ %s ] ", e.name());
+        }
+        return Input.getStringInput();
+    }
+}
+
+
