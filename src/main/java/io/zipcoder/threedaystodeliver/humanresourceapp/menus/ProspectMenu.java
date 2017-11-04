4120e00f0f4aa2daed6438c5af520ef64e8ec757
rikeroleg <rikeroleg@gmail.com>
2017-11-04 13:49:24 -0400

    very minor clean up

--- src/main/java/io/zipcoder/threedaystodeliver/humanresourceapp/menus/ProspectMenu.java
+++ src/main/java/io/zipcoder/threedaystodeliver/humanresourceapp/menus/ProspectMenu.java
@@ -119,7 +119,7 @@ public class ProspectMenu extends Menu{
     }
 
     private void setActiveProspect() {
-        String input = "";
+        String input;
         do {
             System.out.println("Find by [ID] or [Name]?");
             input = this.getUserInput();
